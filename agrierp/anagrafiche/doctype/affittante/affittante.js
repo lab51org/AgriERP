29aab19c07d7453f8494b996cbf6c3e2c210c73d
Matteo D <md@lab51.org>
2020-10-07 23:11:04 +0200

    added affittante and affittante_lista docTypes

---
+++ agrierp/anagrafiche/doctype/affittante/affittante.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Lab51.org and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Affittante', {
+	// refresh: function(frm) {
+
+	// }
+});
